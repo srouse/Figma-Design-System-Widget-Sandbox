61ddac7469f8545e23d1712de88a8170c209fe50
Scott Rouse <scott.rouse@gmail.com>
2023-10-20 08:15:48 -0500

    Create file: "transformations/fds-web/css-vars.d.ts"

---
+++ transformations/fds-web/css-vars.d.ts
@@ -0,0 +1,85 @@
+export type FDSWSColorValue =
+  'color-grey-000' |
+  'color-grey-100' |
+  'color-grey-200' |
+  'color-grey-300' |
+  'color-grey-400' |
+  'color-grey-500' |
+  'color-grey' |
+  'color-grey-600' |
+  'color-grey-700' |
+  'color-grey-800' |
+  'color-grey-900' |
+  'color-grey-1000';
+
+export type FDSWSSpacingValue =
+  'spacing-col-0' |
+  'spacing-col-0-3' |
+  'spacing-col-0-4' |
+  'spacing-col-0-6' |
+  'spacing-col-0-8' |
+  'spacing-col-0-9' |
+  'spacing-col-1' |
+  'spacing-col-1-3' |
+  'spacing-col-1-4' |
+  'spacing-col-1-6' |
+  'spacing-col-1-8' |
+  'spacing-col-1-9' |
+  'spacing-col-2' |
+  'spacing-col-2-3' |
+  'spacing-col-2-4' |
+  'spacing-col-2-6' |
+  'spacing-col-2-8' |
+  'spacing-col-2-9' |
+  'spacing-col-3' |
+  'spacing-col-3-3' |
+  'spacing-col-3-4' |
+  'spacing-col-3-6' |
+  'spacing-col-3-8' |
+  'spacing-col-3-9' |
+  'spacing-col-4' |
+  'spacing-col-5' |
+  'spacing-col-6' |
+  'spacing-col-7' |
+  'spacing-col-8' |
+  'spacing-col-9' |
+  'spacing-col-10' |
+  'spacing-col-11' |
+  'spacing-col-12' |
+  'spacing-colvw-0' |
+  'spacing-colvw-0-3' |
+  'spacing-colvw-0-4' |
+  'spacing-colvw-0-6' |
+  'spacing-colvw-0-8' |
+  'spacing-colvw-0-9' |
+  'spacing-colvw-1' |
+  'spacing-colvw-1-3' |
+  'spacing-colvw-1-4' |
+  'spacing-colvw-1-6' |
+  'spacing-colvw-1-8' |
+  'spacing-colvw-1-9' |
+  'spacing-colvw-2' |
+  'spacing-colvw-2-3' |
+  'spacing-colvw-2-4' |
+  'spacing-colvw-2-6' |
+  'spacing-colvw-2-8' |
+  'spacing-colvw-2-9' |
+  'spacing-colvw-3' |
+  'spacing-colvw-3-3' |
+  'spacing-colvw-3-4' |
+  'spacing-colvw-3-6' |
+  'spacing-colvw-3-8' |
+  'spacing-colvw-3-9' |
+  'spacing-colvw-4' |
+  'spacing-colvw-5' |
+  'spacing-colvw-6' |
+  'spacing-colvw-7' |
+  'spacing-colvw-8' |
+  'spacing-colvw-9' |
+  'spacing-colvw-10' |
+  'spacing-colvw-11' |
+  'spacing-colvw-12';
+
+export type FDSWSValue =
+  FDSWSColorValue |
+  FDSWSSpacingValue;
